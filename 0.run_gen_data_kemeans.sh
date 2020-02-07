65e273b33d25e7a62305a399e2c8cbec4e4916cf
kyucheolsim <kyucheolsim@users.noreply.github.com>
2020-02-08 01:14:09 +0900

    source ./venv/bin/activate added

--- 0.run_gen_data_kemeans.sh
+++ 0.run_gen_data_kemeans.sh
@@ -3,31 +3,32 @@
 BIN="gen_data_kmeans.py"
 DATA_DIR="./data"
 
+source ./venv/bin/activate
 mkdir -p ${DATA_DIR}
 
 run_sample()
 {
 	echo "generate sample data..."
-	python3 ${BIN} --n_samples=1000 --n_centers=0 --n_features=2 > ${DATA_DIR}/sample_data_kmeans.txt
+	python ${BIN} --n_samples=1000 --n_centers=0 --n_features=2 > ${DATA_DIR}/sample_data_kmeans.txt
 }
 
 run_small()
 {
 	echo "generate small data..."
-	python3 ${BIN} --n_samples=1000 --n_centers=4 --n_features=2 > ${DATA_DIR}/small_data_kmeans.txt
+	python ${BIN} --n_samples=1000 --n_centers=4 --n_features=2 > ${DATA_DIR}/small_data_kmeans.txt
 }
 
 run_medium()
 {
 	echo "generate medium data..."
-	python3 ${BIN} --n_samples=10000 --n_centers=5 --n_features=2 > ${DATA_DIR}/medium_data_kmeans.txt
+	python ${BIN} --n_samples=10000 --n_centers=5 --n_features=2 > ${DATA_DIR}/medium_data_kmeans.txt
 }
 
 
 run_large()
 {
 	echo "generate large data..."
-	python3 ${BIN} --n_samples=100000 --n_centers=10 --n_features=2 > ${DATA_DIR}/large_data_kmeans.txt
+	python ${BIN} --n_samples=100000 --n_centers=10 --n_features=2 > ${DATA_DIR}/large_data_kmeans.txt
 }
 
 case $1 in
